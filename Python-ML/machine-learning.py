45c7a56620c136bc9b81db12b4335323df39dcce
Nick Schupp <67743990+schuppnick@users.noreply.github.com>
2022-07-20 10:55:10 +0200

    Add files via upload

--- Python-ML/machine-learning.py
+++ Python-ML/machine-learning.py
@@ -1,51 +1,40 @@
-#%%
 #importing the libraries
+from timeit import default_timer as timer
+start = timer()
+
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
 from sqlalchemy import create_engine
+from sklearn.datasets import load_iris
 
-#importing the Iris dataset with pandas
-engine = create_engine('postgresql://schupp:Lachnummer-1@schupptest.postgres.database.azure.com:5432/test')
+engine = create_engine('postgresql://username:password@postgresql-db-name.postgres.database.azure.com:5432/database')
 df = pd.read_sql("iris_data", engine)
-df
 
-#exclude classification for later checks
+#exclude results for later checks
 x = df.iloc[:, [1, 2, 3, 4]].values
-
-#Finding the optimum number of clusters for k-means classification
-wcss = []
-
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
-    kmeans.fit(x)
-    wcss.append(kmeans.inertia_)
-    
-#Plotting the results onto a line graph, allowing us to observe 'The elbow'
-plt.plot(range(1, 11), wcss)
-plt.title('The elbow method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS') #within cluster sum of squares
-plt.show()
 
 #Applying kmeans to the dataset / Creating the kmeans classifier
 kmeans = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
 y_kmeans = kmeans.fit_predict(x)
-
-#Visualising the clusters
-plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Iris-setosa')
-plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Iris-versicolour')
-plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Iris-virginica')
-
-#Plotting the centroids of the clusters
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
-plt.legend()
-
+  
 df["cluster"] = y_kmeans
 df.loc[df["cluster"] == 1, "cluster"] = "setosa"
 df.loc[df["cluster"] == 0, "cluster"] = "versicolor"
 df.loc[df["cluster"] == 2, "cluster"] = "virginica"
+    
+#upload log entry
+end = timer()
+date = datetime.now()
+time_taken = end - start
 
-engine = create_engine('postgresql://schupp:Lachnummer-1@schupptest.postgres.database.azure.com:5432/test')
+#dd/mm/YY H:M:S
+dt_string = date.strftime("%d/%m/%Y %H:%M:%S")
+log = {'Name':['DL - Azure Function'],'Timestamp':[dt_string], 'Zeit gebraucht':[time_taken]}
+df_log = pd.DataFrame(log)
+
+#importing the Iris dataset with pandas
+df_log.to_sql('log', con=engine, if_exists='append')
 df.to_sql('iris_data_test', con=engine, if_exists='replace')
